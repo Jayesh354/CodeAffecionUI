2deb7a4d8fc1beb0eca68d5b2881fd0472305714
Jayesh354 <jayesh.rohit@slscorp.com>
2022-01-07 18:48:16 +0530

    inital structure created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PaymentDetailFormComponent } from './payment/payment-details/payment-detail-form/payment-detail-form.component';
+import { PaymentDetailsComponent } from './payment/payment-details/payment-details.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PaymentDetailsComponent,
+    PaymentDetailFormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
